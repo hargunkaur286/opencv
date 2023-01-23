359967a61124fe40a1f92c110d70571ecdfe5f25
hargunkaur286 <hargunkaur286@gmail.com>
2023-01-24 01:16:51 +0530

    added chapter9

---
+++ chapter8.py
@@ -0,0 +1,16 @@
+# #contour/shape detection
+# import cv2
+# import numpy as np
+
+# path = 'resources/bee.png'
+# img = cv2.imread(path)
+
+# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+
+# cv2.imshow("Original", img)
+# cv2.imshow("Gray image", imgGray)
+# cv2.imshow("Blur image", imgBlur)
+
+# cv2.waitKey(0)
+
